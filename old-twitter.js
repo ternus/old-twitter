48dd080c2f9a706d9c54efd150f2787dcc5a2f45
Christian Ternus <ternus@cternus.net>
2017-06-15 14:43:54 -0400

    Add homepage

--- old-twitter.js
+++ old-twitter.js
@@ -1,10 +1,11 @@
 // ==UserScript==
 // @name         Old Twitter
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.1.1
 // @description  Remove new rounded Twitter styling
 // @author       ternus
 // @match        https://twitter.com/
+// @homepage     https://github.com/ternus/old-twitter
 // @grant        none
 // @run-at       document-body
 // ==/UserScript==
