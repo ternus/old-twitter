77ddb13a3020d4be30d72d5057ca6e8d871fccaf
Christian Ternus <ternus@cternus.net>
2017-06-15 15:16:27 -0400

    Don't use jQuery

--- old-twitter.js
+++ old-twitter.js
@@ -1,15 +1,15 @@
 // ==UserScript==
 // @name         Get Old Twitter Back
 // @namespace    https://cternus.net
-// @version      0.1.4
+// @version      0.1.5
 // @description  Remove new rounded Twitter styling
 // @author       ternus
 // @match        https://twitter.com/*
 // @homepage     https://github.com/ternus/old-twitter
-// @run-at       document-end
+// @run-at       document-body
 // @grant        none
 // ==/UserScript==
 
 (function() {
-    $('body').removeClass('edge-design');
+    document.body.classList.remove('edge-design');
 })();
