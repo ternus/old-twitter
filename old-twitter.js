b208334e6e9ad2c7072df178db34b75cf32edd6a
Christian Ternus <ternus@cternus.net>
2017-06-15 15:01:01 -0400

    Add an all-important asterisk.

--- old-twitter.js
+++ old-twitter.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         Get Old Twitter Back
 // @namespace    https://cternus.net
-// @version      0.1.3
+// @version      0.1.4
 // @description  Remove new rounded Twitter styling
 // @author       ternus
-// @match        https://twitter.com/
+// @match        https://twitter.com/*
 // @homepage     https://github.com/ternus/old-twitter
 // @run-at       document-end
 // @grant        none
