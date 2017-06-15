a54dced30bb42372e5bbaaa1cbd9d77783fcacc7
Christian Ternus <ternus@cternus.net>
2017-06-15 14:50:43 -0400

    Remove 'document-body'; not necessary

--- old-twitter.js
+++ old-twitter.js
@@ -1,13 +1,12 @@
 // ==UserScript==
-// @name         Old Twitter
+// @name         Get Old Twitter Back
 // @namespace    http://tampermonkey.net/
-// @version      0.1.1
+// @version      0.1.2
 // @description  Remove new rounded Twitter styling
 // @author       ternus
 // @match        https://twitter.com/
 // @homepage     https://github.com/ternus/old-twitter
 // @grant        none
-// @run-at       document-body
 // ==/UserScript==
 
 (function() {
